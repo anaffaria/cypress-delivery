d9dc4a8faadf929151ba647a79a5e8921b441db2
anafsilva <anacarolinq.ng@gmail.com>
2021-12-27 18:21:53 -0300

    matching css selector with text

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,47 @@
+describe('Cadastro', ()=>{
+
+    it('Usuário deve se tornar um entregador', ()=>{
+        cy.viewport(1440,900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Fernando Papito',
+            cpf: '00000001414',
+            email: 'ana@gmail.com',
+            whatsapp:'11989863523',
+            endereco:{
+                cep: '08570350',
+                rua: 'Rua Mooca',
+                numero: '1000',
+                complemento: 'Ap 142',
+                bairro: 'Vila Sônia',
+                cidade_uf: 'Itaquaquecetuba/SP',
+            },
+            metodo_entrega: 'Moto'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        
+
+    })
+    
+    
+})
