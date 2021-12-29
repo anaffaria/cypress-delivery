4b83db09b4fa74ca0919887d3892517947e9cf1b
anafsilva <anacarolinq.ng@gmail.com>
2021-12-29 19:20:58 -0300

    fix(integration/cadastro): created test case user with cpf invalid

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,6 +1,6 @@
 describe('Cadastro', ()=>{
 
-    it('Usuário deve se tornar um entregador', ()=>{
+    it('Usuário se torna um entregador', ()=>{
         cy.viewport(1440,900)
         cy.visit('https://buger-eats.vercel.app')
 
@@ -49,5 +49,50 @@ describe('Cadastro', ()=>{
 
     })
     
-    
+    it('Usuário informa CPF inválido', ()=>{
+        cy.viewport(1440,900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Ana Caroline',
+            cpf: '00000001414AA',
+            email: 'ana@gmail.com',
+            whatsapp:'11989863523',
+            endereco:{
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '100',
+                complemento: 'Ap 142',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP',
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        
+        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
+        cy.get('form button[type="submit"]').click()
+
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+    })
 })
